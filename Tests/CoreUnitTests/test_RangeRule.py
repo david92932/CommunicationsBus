cda58ca68c69f2d41f8ff004d4853f08186bddb7
gfj5035 <60010153+gfj5035@users.noreply.github.com>
2021-05-04 16:42:00 -0400

    Fixed commented out tests in test_DefinedValuesRule,test_Field,test_RangeRule

--- Tests/CoreUnitTests/test_RangeRule.py
+++ Tests/CoreUnitTests/test_RangeRule.py
@@ -11,29 +11,32 @@ class test_RangeRule(unittest.TestCase):
         self.testRule = RangeRule("99.0.0.0", 0, 250, 2, 1)
 
     def test_CheckValidValues(self):
+        testRule = RangeRule("99.0.0.0", 0, 250, 2, 1)
 
-        assert True
-        # #invalid min value
-        # value_is_valid, message = self.testRule.checkValidValues(-6)
-        # assert value_is_valid == False
-        # assert isinstance(message, str)
-        #
-        # #invalid max value
-        # value_is_valid, message = self.testRule.checkValidValues(252)
-        # assert value_is_valid == False
-        # assert isinstance(message, str)
-        #
-        # #invalid not divisible
-        # value_is_valid, message = self.testRule.checkValidValues(51)
-        # assert value_is_valid == False
-        # assert isinstance(message, str)
-        #
-        # #invalid, too big for field
-        # value_is_valid, message = self.testRule.checkValidValues(3000)
-        # assert value_is_valid == False
-        # assert isinstance(message, str)
-        #
-        # #valid
-        # value_is_valid, message = self.testRule.checkValidValues(100)
-        # print(message)
-        # assert value_is_valid == True
+        #invalid min value
+        value_is_valid = testRule.checkValidValues(-6)
+        assert value_is_valid[0]["Valid"] == False
+        assert isinstance(value_is_valid[0]["message"], str)
+
+
+        #invalid max value
+        value_is_valid= testRule.checkValidValues(252)
+        assert value_is_valid[0]["Valid"] == False
+        assert isinstance(value_is_valid[0]["message"], str)
+
+        #invalid not divisible
+        value_is_valid= testRule.checkValidValues(51)
+        print(value_is_valid)
+        assert value_is_valid == []
+
+
+        #invalid, too big for field
+        value_is_valid= testRule.checkValidValues(3000)
+        print(value_is_valid)
+        assert value_is_valid[0]["Valid"] == False
+        assert isinstance(value_is_valid[0]["message"], str)
+
+        #valid
+        value_is_valid= testRule.checkValidValues(100)
+
+        assert value_is_valid == []
